0e4b0b66e9d46fe7d04ea094e1c2a67fc4412edb
Jay Vince Serato <jayvince.serato@gmail.com>
2024-03-05 15:38:39 +0800

    Add BingoGame.java

---
+++ src/Bingo/BingoGame.java
@@ -0,0 +1,23 @@
+package Bingo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BingoGame implements Runnable{
+    List<BingoCard> cards;
+    @Override
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many players? ");
+        int cnt = sc.nextInt();
+        cards = new ArrayList<>();
+        for (int i = 0; i < cnt; i++) {
+            cards.add(new BingoCard(i+1));
+        }
+        for (BingoCard card : cards) {
+            System.out.println("Card " + card.id);
+            System.out.println(card);
+        }
+    }
+}
